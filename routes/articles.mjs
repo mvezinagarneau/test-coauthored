a09db98abe57b3b1d98d9087fd23994019dfc658
mvezina <mvezina@cegepgarneau.ca>
2026-06-08 08:25:42 -0400

    Add Post

--- routes/articles.mjs
+++ routes/articles.mjs
@@ -4,4 +4,6 @@ import * as articleController from "../controllers/articleController.mjs";
 const router = express.Router();
 
 router.get("/", articleController.getArticles);
+
+router.post("/", articleController.createArticle);
 export default router;
