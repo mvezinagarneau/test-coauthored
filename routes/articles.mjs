a23d5fb47b35e02c631e177d6685de9592b20fb0
mvezina <mvezina@cegepgarneau.ca>
2026-06-08 08:26:42 -0400

    Remove routes

--- routes/articles.mjs
+++ routes/articles.mjs
@@ -3,7 +3,4 @@ import * as articleController from "../controllers/articleController.mjs";
 
 const router = express.Router();
 
-router.get("/", articleController.getArticles);
-
-router.post("/", articleController.createArticle);
 export default router;
