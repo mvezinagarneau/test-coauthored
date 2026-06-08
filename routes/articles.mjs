abf0d67492892a017c1b1219a429fcc83a94a539
mvezina <mvezina@cegepgarneau.ca>
2026-06-08 08:24:02 -0400

    Remove all function

--- routes/articles.mjs
+++ routes/articles.mjs
@@ -3,17 +3,4 @@ import * as articleController from "../controllers/articleController.mjs";
 
 const router = express.Router();
 
-// GET /articles - Retourne la liste de tous les articles
-router.get("/", articleController.getArticles);
-
-// GET /articles/:id - Retourne les détails d'un article spécifique en fonction de son ID
-router.get("/:id", articleController.getArticleById);
-
-router.post("/", articleController.createArticle);
-
-// PUT /articles/:id - Met à jour un article spécifique en fonction de son ID
-router.put("/:id", articleController.updateArticle);
-
-router.delete("/:id", articleController.deleteArticle);
-
 export default router;
