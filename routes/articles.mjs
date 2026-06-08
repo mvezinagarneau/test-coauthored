c9a2abf1f54c15dd566aa74da54d59deb37447e8
mvezina <mvezina@cegepgarneau.ca>
2026-06-08 08:33:58 -0400

    Post

--- routes/articles.mjs
+++ routes/articles.mjs
@@ -7,4 +7,5 @@ router.get("/", articleController.getArticles);
 router.get("/:id", articleController.getArticleById);
 router.delete("/:id", articleController.deleteArticle);
 router.put("/:id", articleController.updateArticle);
+router.post("/", articleController.createArticle);
 export default router;
